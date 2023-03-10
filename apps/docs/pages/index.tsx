cd266c50615aa21d623bc31828835db611679c43
Daniel Rios Pavia <daniel.riospavia@shopify.com>
2023-03-10 19:28:40 +0100

    this is an update

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { add, Button } from "ui";
 export default function Docs() {
   return (
     <div>
-      <h1>Docs {add(1, 1)}</h1>
+      <h1>Docs {add(1, 1)} this is an update</h1>
       <Button />
     </div>
   );
